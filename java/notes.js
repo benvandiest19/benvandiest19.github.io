47cb70998b86695d035b90074f2e81742c703814
benvandiest19 <beva7285@colorado.edu>
2022-03-29 11:02:16 -0600

    Week 11 Demo - Java

---
+++ java/notes.js
@@ -0,0 +1,54 @@
+let myVariable;
+myVariable = 'Ben';
+
+let myVariable = 'Jayde';
+
+myVariable = 'Kaia'
+
+// Single Line Comment
+/*  Multi-Line Comment
+*/
+
+let myVarialbe = 'bob'; // String
+
+let myNumber = 3; // Numbers
+
+let myBoolean = True; //Boolean
+let myBoolean = False;
+
+let myArray = [1, 'bob', True]; //Arrays
+
+let myObject = document.querySelector('p')
+//Objects
+
+
+//OPERATORS
+6 + 9; //Addition
+'Hello' + 'World' //String Addition --> 'HelloWorld'
+//Subtraction --> -
+//Multiplication --> *
+//Division --> /
+//Assignment --> =
+//OPERATORS ^^
+
+myNumber ===4; //Equality --> Result in False
+
+!(myNumber ===3); //Not --> Result in False
+myVariable !==3; // Does-Not-Equal --> Result in False
+
+
+//CONDITIONALS
+let iceCream = 'chocolate';
+
+if(iceCream === 'chocolate') {
+  alert("Yay, I love chocolate!");
+} else {
+  alert("Aw, but chocolate is my favorite");
+}
+//CONDITIONALS ^^
+
+// EVENT LISTENERS
+document.querySelector('html').addEventListener('click',
+function() {
+  alert("Ouch! Stop poking me!");
+})
