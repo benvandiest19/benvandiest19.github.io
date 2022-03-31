438b2ab6dd76cf31eadf03cf56c6be2419df338d
benvandiest19 <beva7285@colorado.edu>
2022-03-31 12:01:23 -0600

    Assignment 11

--- java/script.js
+++ java/script.js
@@ -1,8 +1,29 @@
-const para = document.querySelector('p'):
+// document.querySelector('.next').style.display = 'none';
+// document.querySelector('.sushi').addEventListener('click', showBtn);
+//
+// function showBtn(e) {
+//  document.querySelector('.next').style.display = 'block';
+//  e.preventDefault();
+// }
 
-para.addEventListener('click', updateName);
+document.getElementById("sushi").addEventListener("click", sushi);
+function sushi() {
+  document.write("Oh Wow, you've got some taste! That's my favorite! Must have quite the wallet.");
+}
 
-function updateName () {
-  const name = prompt('Enter New Name');
-  para.textContent = `Player 1: ${name}`;
+document.getElementById('pizza').addEventListener("click", pizza);
+function pizza() {
+  document.querySelector("#pizza").innerHTML = "Yum!";
+
+}
+
+document.getElementById("hamburger").addEventListener("click", hamburger);
+function hamburger() {
+  document.querySelector('#hamburger').style.fontSize="35px";
+}
+
+
+document.getElementById("taco").addEventListener("click", taco);
+function taco() {
+  alert("My favorite taco place is Torchy's Tacos! You should try it!");
 }
