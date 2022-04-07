eadd8ec0cc81bf9989eae0c2d7c949481335ee02
benvandiest19 <beva7285@colorado.edu>
2022-04-07 12:07:32 -0600

    week 12 assignment

--- Week12/week12-demo.js
+++ Week12/week12-demo.js
@@ -1,27 +1,56 @@
 // document.querySelector('button');
 // document.querySelector('.new-quote button');
-function getQuote() {
-  console.log("quote button was clicked"); //Used for Debugging "Inspect"
-  try{
+
+async function getQuote() {
+  console.log("quote button was clicked");
+  try {
     const response = await fetch(endpoint);
-    if (!response.ok) {          // if response is not okay, then...
-      throw Error(response.statusText) // error we want to appear in console
+    if (!response.ok) {
+      throw Error(response.statusText)
     }
+
     const json = await response.json();
     console.log(json);
-    displayQuote(json.fact);
-  } catch(err) {
+    displayQuote(json.content);
+    // displayAuthor(json.author);
+  }catch(err) {
     console.log(err)
-    alert("Failed");
+    alert('Failed');
   }
 }
 
 function displayQuote(quote) {
-  const quoteText = document.querySelector('#js-quote-text')
+  const quoteText = document.querySelector('#js-quote-text');
   quoteText.textContent = quote;
 }
 
-const endpoint = "https://catfact.ninja/fact";
+const endpoint = 'https://api.quotable.io/random';
+const quoteButton = document.querySelector('#js-new-quote');
+quoteButton.addEventListener('click', getQuote)
 
-const quoteButton = document.querySelector("#js-new-quote");
-quoteButton.addEventListener("click", getQuote);
+
+// async function getAuthor() {
+//   console.log("quote button was clicked");
+//   try {
+//     const response = await fetch(endpoint);
+//     if (!response.ok) {
+//       throw Error(response.statusText)
+//     }
+//
+//     const json = await response.json();
+//     console.log(json);
+//     displayAuthor(json.author);
+//   }catch(err) {
+//     console.log(err)
+//     alert('Failed');
+//   }
+// }
+//
+// function displayAuthor(author) {
+//   const authorText = document.querySelector('#js-author-text');
+//   authorText.textContent = author;
+// }
+//
+// const endpoint = 'https://api.quotable.io/random';
+// const quoteButton = document.querySelector('#js-new-quote');
+// quoteButton.addEventListener('click', getAuthor)
