86011a4a5def43659711b74127b40d666660eed8
benvandiest19 <beva7285@colorado.edu>
2022-04-14 12:05:23 -0600

    final work pt. 3

--- java/final.js
+++ java/final.js
@@ -1,19 +1,14 @@
 (function(){
   function buildQuiz(){
-    // variable to store the HTML output
     const output = [];
 
-    // for each question...
     myQuestions.forEach(
       (currentQuestion, questionNumber) => {
 
-        // variable to store the list of possible answers
         const answers = [];
 
-        // and for each available answer...
         for(letter in currentQuestion.answers){
 
-          // ...add an HTML radio button
           answers.push(
             `<label>
               <input type="radio" name="question${questionNumber}" value="${letter}">
@@ -23,7 +18,6 @@
           );
         }
 
-        // add this question and its answers to the output
         output.push(
           `<div class="question"> ${currentQuestion.question} </div>
           <div class="answers"> ${answers.join('')} </div>`
@@ -31,43 +25,51 @@
       }
     );
 
-    // finally combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
   }
 
   function showResults(){
 
-    // gather answer containers from our quiz
     const answerContainers = quizContainer.querySelectorAll('.answers');
 
-    // keep track of user's answers
     let numCorrect = 0;
 
-    // for each question...
     myQuestions.forEach( (currentQuestion, questionNumber) => {
 
-      // find selected answer
       const answerContainer = answerContainers[questionNumber];
       const selector = `input[name=question${questionNumber}]:checked`;
       const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      // if answer is correct
       if(userAnswer === currentQuestion.correctAnswer){
-        // add to the number of correct answers
         numCorrect++;
 
-        // color the answers green
-        answerContainers[questionNumber].style.color = 'lightgreen';
+        answerContainers[questionNumber].style.color = "#f18076";
       }
-      // if answer is wrong or blank
       else{
-        // color the answers red
-        answerContainers[questionNumber].style.color = 'red';
+        answerContainers[questionNumber].style.color = "#CC128C";
       }
     });
 
-    // show number of correct answers out of total
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+
+    if (numCorrect == 0){
+      alert("Huh, looks like you aren't even close to being my best friend. But hey, there is still a chance...right?")
+    }
+    if (numCorrect == 1){
+      alert("Okay... you didn't completely fail, but also didn't do well. Like at all.")
+    }
+    if (numCorrect == 2){
+      alert("We don't really have much in common. We -might- find each other at the same (insert name here) convention.")
+    }
+    if (numCorrect == 3){
+      alert("I will call you my acquaintance. Not a stranger, yet not a friend. Just -Acquaintance-")
+    }
+    if (numCorrect == 4){
+      alert("Not too bad! You have guaranteed a birthday party invite (if this were 5th grade).")
+    }
+    if (numCorrect == 5){
+      alert("Where have you been all my life!? We must be best friends! *Disclaimer: These 5 questions are not at all indicative of our true friendship. You must pass the guantlet to be a true friend...*")
+    }
   }
 
   const quizContainer = document.getElementById('quiz');
@@ -126,9 +128,7 @@
     }
   ]
 
-  // Kick things off
   buildQuiz();
 
-  // Event listeners
   submitButton.addEventListener('click', showResults);
 })();
